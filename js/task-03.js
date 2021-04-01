051a387421719a09a55c18c9e3476da253c2c7b8
OleksandrVasylchuk <73894374+OleksandrVasylchuk@users.noreply.github.com>
2021-04-02 00:34:22 +0300

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -5,31 +5,37 @@
 // Добавь минимальное оформление галереи флексбоксами или гридами через css - классы.
 
 const images = [
-  {
-    url:
-      'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
-  },
-  {
-    url:
-      'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
-  },
-  {
-    url:
-      'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
-  },
+    {
+        url:
+            'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        alt: 'White and Black Long Fur Cat',
+    },
+    {
+        url:
+            'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    },
+    {
+        url:
+            'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        alt: 'Group of Horses Running',
+    },
 ];
- const item = document.createElement("li")
-const elementImages = images.map(imgs => {
 
-    const images = document.createElement("img");
-    images.src = imgs.url;
-    images.alt = imgs.alt;
-    item.appendChild(images);
-    console.log(images)
-    return images;
-});
-images.insertAdjacentHTML('afterbegin', 'item')
-document.body.appendChild(item);
+const imagesContainer = document.querySelector("#gallery");
+const elementImages = ({ url, alt }) => {
+    return `
+    <li class="item"><img width="400px" height="300px" src="${url}" alt="${alt}"></li>
+     `;
+};
+   
+
+
+const imagestemplateStrings = images.map(elementImages).join(" ");
+console.log(imagestemplateStrings)
+
+imagesContainer.insertAdjacentHTML('afterbegin', imagestemplateStrings)
+console.log(imagesContainer)
+
+imagesContainer.style.display = "flex";
+
